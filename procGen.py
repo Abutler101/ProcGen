7785b424e93fa812a327af64c8db3d907ae188b9
Abutler101 <alexbutler1810@hotmail.com>
2017-06-17 09:27:54 +0100

    added first block
    double enemy all sides

--- procGen.py
+++ procGen.py
@@ -1,4 +1,9 @@
 #[TIME,TYPE_OF_SQUARE,DIRECTION,SPEED]
+type= ["enemy","friend"]
+dirs=["D","L","U","R"]
+sp=55
+level=[]
+#Template
 '''
 {
     "rankC": 1,
@@ -7,7 +12,7 @@
     "rankA+": 280,
     "1":
     [
-        [],
+        [4.0,"enemy",],
         [],
         [],
         [],
@@ -23,3 +28,16 @@
         [],
         [],
     ],
+'''
+##########################Pre-built blocks
+def DEAS(x):
+    DEAS=[
+        [4.00, "enemy", "D", 55],
+        [4.25, "enemy", "D", 55],
+        [4.50, "enemy", "L", 55],
+        [4.75, "enemy", "L", 55],
+        [5.00, "enemy", "U", 55],
+        [5.25, "enemy", "U", 55],
+        [5.50, "enemy", "R", 55],
+        [5.75, "enemy", "R", 55]
+        ]
